12f876bcd2b2276ed97d74cf75905b2dcfd9fc86
vinh tran <vinhjs@gmail.com>
2020-04-10 13:49:31 +0700

    post message

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
 app.get('/webhook', (req, res) => {
 
 	// Your verify token. Should be a random string.
-	let VERIFY_TOKEN = "qup@abc"
+	let VERIFY_TOKEN = process.env.VERIFY_TOKEN || "qup@abc"
 			
 	// Parse the query params
 	let mode = req.query['hub.mode'];
@@ -41,6 +41,45 @@ app.get('/webhook', (req, res) => {
 			res.sendStatus(403);      
 			}
 	}
+});
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender ID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        // handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        
+        // handlePostback(sender_psid, webhook_event.postback);
+      }
+      
+    });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
